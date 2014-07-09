aa61fe0d1015fd9a12c3d9f9dc1867dda686b633
Ting-Yu Lu <styhihs@styhihs.local>
2014-07-10 00:08:38 +0800

    Done with Part VI: Profile Page And Avatars

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm
+from forms import LoginForm, EditForm
 from models import User, ROLE_USER, ROLE_ADMIN
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -37,37 +38,60 @@ def login():
 @oid.after_login
 def after_login(resp):
     # resp: information returned by the OpenID provider
-    print 'resp.email: ' + resp.email
     if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
-    print 'herererere'
-    print User.query.all()
     
     user = User.query.filter_by(email = resp.email).first()
-    print 'ohohohohohohohohoh'
     # New user
     if user is None:
-        print 'add new user!!!'
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
         user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
         db.session.add(user)
         db.session.commit()
-    print 'NOT NEW USER'
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
     login_user(user, remember = remember_me)
-    print 'gooooooooooooooooooooood'
     return redirect(request.args.get('next') or url_for('index'))
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    posts = [
+        { 'author': user, 'body': 'Test post #1' },
+        { 'author': user, 'body': 'Test post #2' }
+    ]
+    return render_template('user.html', user = user, posts = posts)
+
+@app.route('/edit', methods = ['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form = form)
+
 
 @lm.user_loader
 def load_user(id):
@@ -76,3 +100,7 @@ def load_user(id):
 @app.before_request
 def before_request():
     g.user = current_user   # current_user is set by Flask-Login
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
