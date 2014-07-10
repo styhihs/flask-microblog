9be60b6f4e7ca0519ec988c7fb23dd669b809548
styhihs <styhihs@gmail.com>
2014-07-10 08:13:40 +0800

    Done with Part VII: Unit Testing

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com'}
 ]
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -14,3 +15,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 # folder to store the SQLAlchemy-migrate data files
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['styhihs@gmail.com']
